c9a05192879adf7a5a7d0f4728dddeaf7b22eee2
Gerardo De Leon Garcia <gerardo-leon10@hotmail.com>
2025-02-15 09:52:13 -0600

    Detalles de la película

--- src/presentation/screens/details/DetailsScreen.tsx
+++ src/presentation/screens/details/DetailsScreen.tsx
@@ -1,9 +1,11 @@
 import {StackScreenProps} from '@react-navigation/stack';
 import React from 'react';
-import {Text, View} from 'react-native';
+import {Text} from 'react-native';
 import {RootStackParams} from '../../navigation/Navigation';
 import {useMovie} from '../../hooks/useMovie';
 import {MovieHeader} from '../../components/movie/MovieHeader';
+import {MovieDetails} from '../../components/movie/MovieDetails';
+import {ScrollView} from 'react-native-gesture-handler';
 
 interface Props extends StackScreenProps<RootStackParams, 'Details'> {}
 
@@ -16,7 +18,7 @@ export const DetailsScreen = ({route}: Props) => {
   }
 
   return (
-    <View>
+    <ScrollView>
       {/* Header */}
       <MovieHeader
         originalTitle={movie?.originalTitle!}
@@ -25,6 +27,7 @@ export const DetailsScreen = ({route}: Props) => {
       />
 
       {/* Details */}
-    </View>
+      <MovieDetails movie={movie!} />
+    </ScrollView>
   );
 };
