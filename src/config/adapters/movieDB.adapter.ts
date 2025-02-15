984c7da8decb1790d235169a1c62efec20a2542f
Gerardo De Leon Garcia <gerardo-leon10@hotmail.com>
2025-02-15 10:51:06 -0600

    Variables de entorno en React Native

--- src/config/adapters/movieDB.adapter.ts
+++ src/config/adapters/movieDB.adapter.ts
@@ -1,9 +1,10 @@
+import {THE_MOVIE_DB_KEY} from '@env';
 import {AxiosAdapter} from './http/axios.adapter';
 
 export const movieDBFetcher = new AxiosAdapter({
   baseUrl: 'https://api.themoviedb.org/3/movie',
   params: {
-    api_key: '394ca0da481da4189d934175665b75ff',
+    api_key: THE_MOVIE_DB_KEY ?? 'no-key',
     language: 'es',
   },
 });
